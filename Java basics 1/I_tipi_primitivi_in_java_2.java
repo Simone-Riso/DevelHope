4c71240d78f41204fa1352248e210eb6e180a1a1
Simone Riso <ez.simone.riso@gmail.com>
2024-02-26 19:08:07 +0100

    Create I_tipi_primitivi_in_java_2.java

---
+++ Java basics 1/I_tipi_primitivi_in_java_2.java
@@ -0,0 +1,8 @@
+public class I_tipi_primitivi_in_java_2 {
+    public static void main(String[] args) {
+        char x = 'x';
+        char y = 'y';
+        String combinazione = "" + x + y;
+        System.out.println("La combinazione dei due caratteri è: " + combinazione);
+    }
+}
