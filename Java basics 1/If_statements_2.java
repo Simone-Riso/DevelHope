dc8742a95056f3cfdaedc2b76796e8cd3166bfdc
Simone Riso <ez.simone.riso@gmail.com>
2024-02-26 20:34:43 +0100

    Create If_statements_2.java

---
+++ Java basics 1/If_statements_2.java
@@ -0,0 +1,23 @@
+public class If_statements_2 {
+    public static void main(String[] args) {
+        String str = "Hello, world!";
+        int lunghezza = calcolaLunghezza(str);
+
+        if (lunghezza > 10) {
+            System.out.println("Lunghezza maggiore di 10");
+        } else if (lunghezza < 10) {
+            System.out.println("Lunghezza minore di 10");
+        } else {
+            System.out.println("Lunghezza pari a 10");
+        }
+    }
+
+    public static int calcolaLunghezza(String str) {
+        int lunghezza = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            lunghezza++;
+        }
+        return lunghezza;
+    }
+}
