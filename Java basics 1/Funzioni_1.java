db726fc07503caa4ad1b9afc2a950e63b420e798
Simone Riso <ez.simone.riso@gmail.com>
2024-02-26 19:21:37 +0100

    Create Funzioni_1.java

--- Java basics 1/Funzioni_1.java
+++ Java basics 1/Funzioni_1.java
@@ -1,3 +1,9 @@
 public class Funzioni_1 {
-    
+    public static void main(String[] args) {
+        System.out.println("La somma dei due numeri è: " + somma(1.00, 2.00));
+    }
+
+    public static double somma(double valore1, double valore2) {
+        return valore1 + valore2;
+    }
 }
