a1f1398fcba2f70d8fd9d579604d76101e4b73f4
Simone Riso <ez.simone.riso@gmail.com>
2024-02-26 19:11:18 +0100

    Create Casting_1.java

---
+++ Java basics 1/Casting_1.java
@@ -0,0 +1,8 @@
+public class Casting_1 {
+    public static void main(String[] args) {
+        double x = 3.00;
+        double y = 2.50;
+        int somma = (int) (x + y);
+        System.out.println("La somma dei due numeri è: " + somma);
+    }
+}
