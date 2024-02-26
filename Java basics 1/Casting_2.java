584f04b3493f1cac14ac498ce4eaac8fb7ef885b
Simone Riso <ez.simone.riso@gmail.com>
2024-02-26 19:12:30 +0100

    Create Casting_2.java

---
+++ Java basics 1/Casting_2.java
@@ -0,0 +1,10 @@
+public class Casting_2 {
+    public static void main(String[] args) {
+        char A = 'A';
+        char B = 'B';
+
+        int sumAscii = (int) A + (int) B;
+
+        System.out.println("La somma dei valori ASCII di " + A + " e " + B + " è: " + sumAscii);
+    }
+}
