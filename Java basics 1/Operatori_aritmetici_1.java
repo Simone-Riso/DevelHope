480eb8ee2c0ec16e441cb58900f10f57eae2831e
Simone Riso <ez.simone.riso@gmail.com>
2024-02-26 19:27:14 +0100

    Create Operatori_aritmetici_1.java

---
+++ Java basics 1/Operatori_aritmetici_1.java
@@ -0,0 +1,9 @@
+public class Operatori_aritmetici_1 {
+    public static void main(String[] args) {
+        System.out.println("Quoziente:" + quoziente(1, 2));
+    }
+
+    public static double quoziente(int valore1, int valore2) {
+        return valore1 % valore2;
+    }
+}
