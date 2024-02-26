8af2166ac2d900aeacb982f8e7f067ddb90ae1a9
Simone Riso <ez.simone.riso@gmail.com>
2024-02-26 19:51:38 +0100

    Create Operatori_logici_2.java

---
+++ Java basics 1/Operatori_logici_2.java
@@ -0,0 +1,10 @@
+public class Operatori_logici_2 {
+    public static void main(String[] args) {
+        int numero = 10;
+        System.out.println(numero + " è " + (èPari(numero) ? "pari" : "dispari"));
+    }
+
+    public static boolean èPari(int numero) {
+        return ((numero & 1) == 0);
+    }
+}
