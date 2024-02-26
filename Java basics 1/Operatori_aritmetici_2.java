0183bac97d71aa19a9870e7dbd1ea2c03ebf1440
Simone Riso <ez.simone.riso@gmail.com>
2024-02-26 19:33:05 +0100

    Create Operatori_aritmetici_2.java

---
+++ Java basics 1/Operatori_aritmetici_2.java
@@ -0,0 +1,9 @@
+public class Operatori_aritmetici_2 {
+    public static void main(String[] args) {
+        System.out.println("Media: " + media(1, 2, 3));
+    }
+
+    public static double media(int valore1, int valore2, int valore3) {
+        return (double) (valore1 + valore2 + valore3);
+    }
+}
