5c60eefc0c194f669d3f06d4801f2d135b09c202
Simone Riso <ez.simone.riso@gmail.com>
2024-02-26 20:59:56 +0100

    Create While_loop_1.java

---
+++ Java basics 1/While_loop_1.java
@@ -0,0 +1,15 @@
+public class While_loop_1 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int sumOfValues(int upperBound) {
+        int i = 0;
+        int sum = 0;
+
+        while (i < upperBound) {
+            sum += 1;
+            i++;
+        } return sum;
+    }
+}
