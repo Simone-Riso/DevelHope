ec63ce53f7f98ce04d1716236de90847df77f479
Simone Riso <ez.simone.riso@gmail.com>
2024-02-26 21:40:31 +0100

    Create Array_1.java

---
+++ Java basics 1/Array_1.java
@@ -0,0 +1,21 @@
+public class Array_1 {
+    public static void main(String[] args) {
+        int[] numbers = createNumbersArray();
+        System.out.println("Somma degli elementi: " + calcoloSomma(numbers));
+    }
+
+    public static int[] createNumbersArray() {
+        int[] numbers = new int[10];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i + 1;
+        } return numbers;
+    }
+
+    public static int calcoloSomma(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        } return sum;
+    }
+}
