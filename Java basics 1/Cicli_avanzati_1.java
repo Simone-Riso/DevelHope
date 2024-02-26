c8bd6ecd36eb1b3227d370d518102e54567de8b8
Simone Riso <ez.simone.riso@gmail.com>
2024-02-26 21:11:18 +0100

    Create Cicli_avanzati_1.java

---
+++ Java basics 1/Cicli_avanzati_1.java
@@ -0,0 +1,16 @@
+public class Cicli_avanzati_1 {
+    public static void main(String[] args) {
+        int valore = 10;
+        stampaNumeri(valore);
+    }
+
+    public static void stampaNumeri(int valore) {
+        for (int i = 1; i <= valore; i++) {
+            if (i == 5) {
+                System.out.println("Interrotto perché valore uguale a 5.");
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
