0d2f30e3c87a2d535083235dda55ad6d7e4c001a
Simone Riso <ez.simone.riso@gmail.com>
2024-02-26 20:50:58 +0100

    Update Switch_statement_2.java

--- Java basics 1/Switch_statement_2.java
+++ Java basics 1/Switch_statement_2.java
@@ -1,3 +1,28 @@
 public class Switch_statement_2 {
-    
+    public static void main(String[] args) {
+        char operatore = '-';
+        System.out.println("L'operzione del carattere (" + operatore + ") è: " + arithmeticOperator(operatore));
+    }
+
+    public static String arithmeticOperator(char operatore) {
+        String result;
+
+        switch (operatore) {
+            case '+':
+                result = "Addizzione";
+                break;
+            case '-':
+                result = "Sottrazione";
+                break;
+            case '*':
+                result = "Moltiplicazione";
+                break;
+            case '/':
+                result = "Divisione";
+                break;
+            default:
+                result = "Errore";
+        }
+        return result;
+    }
 }
