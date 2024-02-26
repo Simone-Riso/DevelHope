c36dc35c26c0c58f1ff632543134fb7797dd2742
Simone Riso <ez.simone.riso@gmail.com>
2024-02-26 19:44:34 +0100

    Create Operatori_di_comparazione_2.java

---
+++ Java basics 1/Operatori_di_comparazione_2.java
@@ -0,0 +1,13 @@
+public class Operatori_di_comparazione_2 {
+    public static void main(String[] args) {
+        char carattere1 = 'x';
+        char carattere2 = 'y';
+        System.out.println("Il primo carattere è: " + carattere1);
+        System.out.println("Il secondo carattere è: " + carattere2);
+
+    }
+
+    public static boolean areEquals(char carattere1, char carattere2) {
+        return carattere1 == carattere2;
+    }
+}
