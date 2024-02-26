a65a8473b672944112b2215cdd95d432a91d0c1a
Simone Riso <ez.simone.riso@gmail.com>
2024-02-26 18:41:49 +0100

    Create I_tipi_primitivi_in_Java_1.java

---
+++ Java basics 1/I_tipi_primitivi_in_Java_1.java
@@ -0,0 +1,10 @@
+public class I_tipi_primitivi_in_Java_1 {
+    public static void main(String[] args) {
+        double base = 5.0;
+        double altezza = 5.0;
+
+        double area = (base * altezza) / 2;
+
+        System.out.println("L'area del triangolo è: " + area);
+    }
+}
