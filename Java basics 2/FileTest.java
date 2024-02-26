98e5c510f9ef3f5b47153832279cdb290e24b68b
Simone Riso <ez.simone.riso@gmail.com>
2024-02-26 22:26:38 +0100

    Create FileTest.java

---
+++ Java basics 2/FileTest.java
@@ -0,0 +1,10 @@
+public class FileTest {
+    public static void main(String[] args) {
+        double baseTriangolo = 5.0;
+        double altezzaTriangolo = 5.0;
+
+        double areaTriangolo = (baseTriangolo * altezzaTriangolo) / 2;
+
+        System.out.println("L'area del triangolo è: " + areaTriangolo);
+    }
+}
