0453a2eaf71bb7cc03eb94e1204d8dd82e55de2a
Simone Riso <ez.simone.riso@gmail.com>
2024-02-27 11:46:45 +0100

    Update While_loop_1.java

--- Java basics 1/While_loop_1.java
+++ Java basics 1/While_loop_1.java
@@ -1,8 +1,8 @@
 public class While_loop_1 {
     public static void main(String[] args) {
-
+        System.out.println("La somma è uguale a: " + sumOfValues(5));
     }
-
+    
     public static int sumOfValues(int upperBound) {
         int i = 0;
         int sum = 0;
