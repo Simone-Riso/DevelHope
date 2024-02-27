b66900af18f8265571143605f9605c4dbfce8b25
Simone Riso <ez.simone.riso@gmail.com>
2024-02-27 16:09:20 +0100

    Update For_loop_1.java

--- Java basics 1/For_loop_1.java
+++ Java basics 1/For_loop_1.java
@@ -1,14 +1,14 @@
 public class For_loop_1 {
     public static void main(String[] args) {
-        
+        arithmeticTable(5);
     }
 
     public static void arithmeticTable(int number) {
-        System.out.println("La tabellina del number: " + number);
+        System.out.println("La tabellina del " + number + ":");
 
-        for (int i = 1; i < 11; i++) {
-            int result = number + i;
-            String output = number + "x" + i + "x" + result;
+        for (int i = 1; i <= 10; i++) {
+            int result = number * i;
+            String output = number + " x " + i + " = " + result;
             System.out.println(output);
         }
     }
