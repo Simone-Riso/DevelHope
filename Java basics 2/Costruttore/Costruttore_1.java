a5b47c709e38e2bb57f8178876a4affe794962f6
Simone Riso <ez.simone.riso@gmail.com>
2024-02-28 15:47:55 +0100

    Update file costruttore1

--- Java basics 2/Costruttore/Costruttore_1.java
+++ Java basics 2/Costruttore/Costruttore_1.java
@@ -1,45 +1,8 @@
 package Costruttore;
 
-import java.util.UUID;
-
 public class Costruttore_1 {
     public static void main(String[] args) {
         Studente s = new Studente("Simone", "Riso");
         System.out.println("Lo studente si chiama: " + s.getNome() + " , " + s.getCognome() + " , " + s.getId());
-    }
-
-    static class Studente {
-        String id, nome, cognome;
-
-        public Studente(String nome, String cognome) {
-            this.nome = nome;
-            this.cognome = cognome;
-            this.id = UUID.randomUUID().toString();
-        }
-
-        public String getId() {
-            return id;
-        }
-
-        public void setId(String id) {
-            this.id = id;
-        }
-
-        public String getNome() {
-            return nome;
-        }
-
-        public void setNome(String nome) {
-            this.nome = nome;
-        }
-
-        public String getCognome() {
-            return cognome;
-        }
-
-        public void setCognome(String cognome) {
-            this.cognome = cognome;
-        }
-
     }
 }
