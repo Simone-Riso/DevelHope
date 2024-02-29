e51f0366f2bc6fb220896784f166bb7f0d8c609a
Simone Riso <ez.simone.riso@gmail.com>
2024-02-29 20:09:20 +0100

    update files

--- Java basics 2/Teoria_oop/Articolo.java
+++ Java basics 2/Teoria_oop/Articolo.java
@@ -1,8 +1,30 @@
 package Teoria_oop;
 
 public class Articolo {
-    public String nome;
-    public double prezzo;
+
+    private String nome;
+    private double prezzo;
+
+    public Articolo(String nome, double prezzo) {
+        this.nome = nome;
+        this.prezzo = prezzo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
 
     @Override
     public String toString() {
--- Java basics 2/Teoria_oop/Carrello.java
+++ Java basics 2/Teoria_oop/Carrello.java
@@ -3,12 +3,37 @@ package Teoria_oop;
 import java.util.Arrays;
 
 public class Carrello {
-    public double totale;
-    public Articolo[] articoli;
+    private double totale;
+    private Articolo[] articoli;
+
+    public Carrello(Articolo[] articoli) {
+        this.articoli = articoli;
+        calcolaTotale();
+    }
+
+    public double getTotale() {
+        return totale;
+    }
+
+    public Articolo[] getArticoli() {
+        return articoli;
+    }
+
+    public void setArticoli(Articolo[] articoli) {
+        this.articoli = articoli;
+        this.totale = 0.0;
+        calcolaTotale();
+    }
+
+    private void calcolaTotale() {
+        totale = 0;
+        for (int i = 0; i < articoli.length; i++) {
+            totale += articoli[i].getPrezzo();
+        }
+    }
 
     @Override
     public String toString() {
         return "Carrello [Il totale è: " + totale + ", articoli = " + Arrays.toString(articoli) + "]";
     }
-
 }
