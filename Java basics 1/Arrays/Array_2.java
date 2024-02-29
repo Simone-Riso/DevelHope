83c33a82563ec956c7e0b288ea2734a6f25ba29c
Simone Riso <ez.simone.riso@gmail.com>
2024-02-29 15:57:40 +0100

    Update Array_2.java

--- Java basics 1/Arrays/Array_2.java
+++ Java basics 1/Arrays/Array_2.java
@@ -2,18 +2,19 @@ package Arrays;
 public class Array_2 {
     public static void main(String[] args) {
         char[] chars = createCharArray();
-        char charToFind = 'a';
+        char charToFind = 'g';
         System.out.println(
                 "Numero occorenze per il carattere: " + charToFind + " : " + findOccurencies(charToFind, chars));
     }
 
     public static char[] createCharArray() {
-        char[] chars = new char[5];
+        char[] chars = new char[6];
         chars[0] = 'a';
         chars[1] = 'b';
         chars[2] = 'c';
         chars[3] = 'd';
         chars[4] = 'e';
+        chars[5] = 'f';
         return chars;
     }
 
@@ -22,7 +23,9 @@ public class Array_2 {
         for (char carattere1 : chars) {
             if (carattere1 == toFind) {
                 occ++;
-            }
+            } else if (carattere1 != toFind) {
+                System.out.println("Errore");
+            } break;
         }
         return occ;
     }
