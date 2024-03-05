cd5ac81a780f4cc7ab4fd76f8ef3c9236356e741
Simone Riso <ez.simone.riso@gmail.com>
2024-03-05 18:19:38 +0100

    esercizio 2

---
+++ Java basics 3/Ereditarietà_2/Forma.java
@@ -0,0 +1,7 @@
+package Ereditarietà_2;
+
+public class Forma {
+    public void calcolaArea() {
+        System.out.println("Area della forma generica");
+    }
+}
---
+++ Java basics 3/Ereditarietà_2/Main.java
@@ -0,0 +1,10 @@
+package Ereditarietà_2;
+
+public class Main {
+    public static void main(String[] args) {
+        Forma forma = new Forma();
+        forma.calcolaArea();
+        Rettangolo rettangolo = new Rettangolo(3, 5);
+        rettangolo.calcolaArea();
+    }
+}
