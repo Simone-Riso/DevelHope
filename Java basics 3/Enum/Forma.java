fa70431f3fcf03b9702e5e323f40085e08a9261b
Simone Riso <ez.simone.riso@gmail.com>
2024-03-06 18:48:48 +0100

    Update

---
+++ Java basics 3/Enum/Forma.java
@@ -0,0 +1,12 @@
+package Enum;
+
+public class Forma {
+    public void calcolaArea() {
+        System.out.println("Errore: metodo calcolaArea() non implementato.");
+    }
+}
+
+enum TipoForma {
+    TRIANGOLO,
+    RETTANGOLO,
+}
--- Java basics 3/Interfacce/Rettangolo.java
+++ Java basics 3/Interfacce/Rettangolo.java
@@ -9,6 +9,22 @@ public class Rettangolo implements Forma {
         this.altezza = altezza;
     }
 
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getAltezza() {
+        return altezza;
+    }
+
+    public void setAltezza(double altezza) {
+        this.altezza = altezza;
+    }
+
     @Override
     public double calcolaArea() {
         return base * altezza;
