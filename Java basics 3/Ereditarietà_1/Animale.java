f5195551c4885d3fcc471ce22580e54e998809c9
Simone Riso <ez.simone.riso@gmail.com>
2024-03-06 15:27:43 +0100

    Update Animale.java

--- Java basics 3/Ereditarietà_1/Animale.java
+++ Java basics 3/Ereditarietà_1/Animale.java
@@ -2,6 +2,6 @@ package Ereditarietà_1;
 
 public class Animale {
     public void faiIlVerso() {
-        System.out.println("Verso animale generico");
+        System.out.println("Verso animale");
     }
 }
