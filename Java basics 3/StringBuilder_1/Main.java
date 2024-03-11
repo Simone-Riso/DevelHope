e946f05bbdbec55c1ec567d6fa7861b141dd2a4e
Simone Riso <ez.simone.riso@gmail.com>
2024-03-11 09:50:25 +0100

    Update Main.java

--- Java basics 3/StringBuilder_1/Main.java
+++ Java basics 3/StringBuilder_1/Main.java
@@ -2,15 +2,15 @@ package StringBuilder_1;
 
 public class Main {
     public static void main(String[] args) {
-        String str1 = "CIAO";
-        String str2 = "Ciao";
+        String stringa1 = "Ciao";
+        String stringa2 = "ciao";
 
-        System.out.println(compareStrings(str1, str2));
+        System.out.println(compareStrings(stringa1, stringa2));
     }
 
-    public static String compareStrings(String str1, String str2) {
-        StringBuilder sb1 = new StringBuilder(str1);
-        StringBuilder sb2 = new StringBuilder(str2);
+    public static String compareStrings(String stringa1, String stringa2) {
+        StringBuilder sb1 = new StringBuilder(stringa1);
+        StringBuilder sb2 = new StringBuilder(stringa2);
 
         sb1.setCharAt(0, Character.toLowerCase(sb1.charAt(0)));
         sb2.setCharAt(0, Character.toLowerCase(sb2.charAt(0)));
