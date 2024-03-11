7b7cb97284c84ae0e9e4cf116bba736991107d38
Simone Riso <ez.simone.riso@gmail.com>
2024-03-11 09:38:29 +0100

    Update Main.java

--- Java basics 3/StringBuilder_1/Main.java
+++ Java basics 3/StringBuilder_1/Main.java
@@ -3,31 +3,22 @@ package StringBuilder_1;
 public class Main {
     public static void main(String[] args) {
         String str1 = "CIAO";
-        String str2 = "Canguro";
+        String str2 = "Ciao";
 
         System.out.println(compareStrings(str1, str2));
     }
 
     public static String compareStrings(String str1, String str2) {
-        if (str1.equalsIgnoreCase(str2)) {
+        StringBuilder sb1 = new StringBuilder(str1);
+        StringBuilder sb2 = new StringBuilder(str2);
+
+        sb1.setCharAt(0, Character.toLowerCase(sb1.charAt(0)));
+        sb2.setCharAt(0, Character.toLowerCase(sb2.charAt(0)));
+
+        if (sb1.toString().equalsIgnoreCase(sb2.toString())) {
             return "Le due stringhe sono uguali";
         } else {
             return "Le due stringhe sono differenti";
         }
     }
 }
-
-/* public static void main(String[] args) {
-    String str1 = "ciao";
-    String str2 = "CIAO";
-
-    System.out.println(compareStrings(str1, str2));
-}
-
-public static String compareStrings(String str1, String str2) {
-    if (str1.compareTo(str2) == 0) {
-        return "Le due stringhe sono uguali";
-    } else {
-        return "Le due stringhe sono differenti";
-    }
-} */
