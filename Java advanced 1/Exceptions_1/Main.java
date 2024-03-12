3bf47a8bfbce99c9bf597a410fcb95bffeb8be76
Simone Riso <ez.simone.riso@gmail.com>
2024-03-12 18:11:11 +0100

    Update Main.java

--- Java advanced 1/Exceptions_1/Main.java
+++ Java advanced 1/Exceptions_1/Main.java
@@ -1,15 +1,16 @@
 package Exceptions_1;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
         int minimo = 0;
         int massimo = 10;
-        int numero1 = 6;
-        System.out.println("Il numero è nel range: " + nelRange(numero1, minimo, massimo));
+        int numero = 6;
+
+        System.out.println("Il numero è nel range: " + nelRange(numero, minimo, massimo));
     }
 
-    public static boolean nelRange(int numero1, int minimo, int massimo) {
-        if (numero1 >= minimo && numero1 <= massimo) {
+    public static boolean nelRange(int numero, int minimo, int massimo) {
+        if (numero >= minimo && numero <= massimo) {
             return true;
         } else {
             throw new ArithmeticException("Il numero non rientra nel range tra " + minimo + " e " + massimo);
