5095fad66a76f3f6bb1f61314585a54415b56e21
Simone Riso <ez.simone.riso@gmail.com>
2024-03-13 16:20:19 +0100

    Update Main.java

--- Java advanced 1/Nullability/Main.java
+++ Java advanced 1/Nullability/Main.java
@@ -21,7 +21,3 @@ public class Main {
         } return numeratore / denominatore;
     }
 }
-
-// Scrivere una funzione che accetti in input un numeratore e un denominatore
-// Controlli che il numeratore e il denominatore non siano null
-// Usare la funzione appena scritta in due blocchi di codice dove si cattura l'eventuale eccezione.
