04a2e91163c74df1050df5242b43ac261e6a6c48
Simone Riso <ez.simone.riso@gmail.com>
2024-03-13 13:11:58 +0100

    Create Main.java

---
+++ Java advanced 1/Nullability/Main.java
@@ -0,0 +1,24 @@
+package Nullability;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer numeratore = 10;
+        Integer denominatore = 2;
+
+        try {
+            int risultato = calcolaDivisione(numeratore, denominatore);
+            System.out.println("Il risultato della divisione è: " + risultato);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static int calcolaDivisione(Integer numeratore, Integer denominatore) {
+        if (numeratore == null || denominatore == null) {
+            throw new IllegalArgumentException("Errore: Rilevato input null");
+        } else if (denominatore == 0) {
+            throw new IllegalArgumentException("Errore: Impossibile dividere per zero");
+        }
+        return numeratore / denominatore;
+    }
+}
