ae31429e84fd9d5b9d490782c593fb4d46bf2aea
Simone Riso <ez.simone.riso@gmail.com>
2024-03-18 11:45:02 +0100

    Update Main.java

--- Java basics 2/Oggetti_costruttore_getter_setter_1/Main.java
+++ Java basics 2/Oggetti_costruttore_getter_setter_1/Main.java
@@ -3,6 +3,6 @@ package Oggetti_costruttore_getter_setter_1;
 public class Main {
     public static void main(String[] args) {
         Studente s = new Studente("Simone", "Riso");
-        System.out.println("Lo studente si chiama: " + s.getNome() + " , " + s.getCognome() + " , " + s.getId());
+        System.out.println("Lo studente si chiama: " + s.getNome() + " " + s.getCognome() + " , id: " + s.getId());
     }
 }
