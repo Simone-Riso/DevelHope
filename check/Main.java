21470651c80978e5eac019465673383e100382c5
Simone Riso <ez.simone.riso@gmail.com>
2024-03-21 19:27:44 +0100

    Update Main.java

--- check/Main.java
+++ check/Main.java
@@ -2,10 +2,10 @@ package check;
 
 public class Main {
     public static void main(String[] args) {
-        Dottore dottore1 = new Dottore("16102001", "Simone", "Riso", 10, specializzazione.CARDIOLOGO);
-        Dottore dottore2 = new Dottore("16102001", "Emiliano", "Riso", 10, specializzazione.CHIRURGO);
-        Dottore dottore3 = new Dottore("16102001", "Francesco", "Riso", 10, specializzazione.GINECOLOGO);
-        Dottore dottore4 = new Dottore("16102001", "Gianluca", "Riso", 10, specializzazione.PEDIATRA);
+        Dottore dottore1 = new Dottore("16102001", "Simone", "Riso", 10, Specializzazione.CARDIOLOGO);
+        Dottore dottore2 = new Dottore("16102001", "Emiliano", "Riso", 10, Specializzazione.CHIRURGO);
+        Dottore dottore3 = new Dottore("16102001", "Francesco", "Riso", 10, Specializzazione.GINECOLOGO);
+        Dottore dottore4 = new Dottore("16102001", "Gianluca", "Riso", 10, Specializzazione.PEDIATRA);
 
         dottore1.aggiungiPaziente("Paolo");
         dottore1.aggiungiPaziente("Daniel");
