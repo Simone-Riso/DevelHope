573e37764134762f7fa0d9cf72d3e0ceb6bd2cfe
Simone Riso <ez.simone.riso@gmail.com>
2024-03-22 16:59:06 +0100

    Create Studente.java

---
+++ Java advanced 2/ArrayList_2/Studente.java
@@ -0,0 +1,34 @@
+package ArrayList_2;
+
+public class Studente {
+
+    private String name;
+    private int age;
+
+    public Studente(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + name + " " + "Età: " + age;
+    }
+
+}
