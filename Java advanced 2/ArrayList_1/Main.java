9f85904cec6d51ddfa50a11a584d719e6c87cbe1
Simone Riso <ez.simone.riso@gmail.com>
2024-03-22 12:23:03 +0100

    Create Main.java

--- Java advanced 2/ArrayList_1/Main.java
+++ Java advanced 2/ArrayList_1/Main.java
@@ -6,13 +6,19 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Studente> listaStudenti = new ArrayList<>();
 
-        System.out.println("Lista: " + listaStudenti);
+        listaStudenti.add(new Studente("Semen", 9999));
+        listaStudenti.add(new Studente("Michele", 22));
+
+        System.out.println("Lista degli studenti: ");
+        for (Studente studente : listaStudenti) {
+            System.out.println("Nome: " + studente.getName() + " " + "Età: " + studente.getAge());
+        }
 
         listaStudenti.add(new Studente("Simone", 22));
-        listaStudenti.add(new Studente("Emiliano", 22));
-        listaStudenti.add(new Studente("Liborio", 173));
-        listaStudenti.add(new Studente("Emanuele", 24));
-        
+        listaStudenti.add(new Studente("Davide", 23));
+        listaStudenti.add(new Studente("Liborio",404));
+        listaStudenti.add(new Studente("Emanuele", 27));
+
         System.out.println("Lista degli studenti: ");
         for (Studente studente : listaStudenti) {
             System.out.println("Nome: " + studente.getName() + " " + "Età: " + studente.getAge());
