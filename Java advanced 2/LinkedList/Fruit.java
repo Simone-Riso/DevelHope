6b1e578d5074778bca4a1d0e95ac9227cf449ae5
Simone Riso <ez.simone.riso@gmail.com>
2024-03-22 18:24:20 +0100

    Create Fruit.java

---
+++ Java advanced 2/LinkedList/Fruit.java
@@ -0,0 +1,23 @@
+package LinkedList;
+
+public class Fruit {
+    String name;
+
+    public Fruit(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Frutta: " + name;
+    }
+
+}
