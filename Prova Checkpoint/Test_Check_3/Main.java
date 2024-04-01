1973fe3e4edcc33056f96172b5407655aa705352
Simone Riso <ez.simone.riso@gmail.com>
2024-04-01 18:27:00 +0200

    Update Main.java

--- Prova Checkpoint/Test_Check_3/Main.java
+++ Prova Checkpoint/Test_Check_3/Main.java
@@ -3,6 +3,14 @@ package Test_Check_3;
 public class Main {
     public static void main(String[] args) {
         Corso corso1 = new Corso(NomeCorso.INGLESE, 20);
-        corso1.addStudente(new Studente(161001, "Simone", "Riso", "Via blablabla"));
+        
+        corso1.addStudente(new Studente(161001, "Simone", "Riso", "Via yuhahby"));
+        corso1.addStudente(new Studente(161002, "Emiliano", "Riso", "Via lghskjnbkj"));
+        corso1.addStudente(new Studente(161003, "Carlo", "Riso", "Via abckbkfb"));
+
+        corso1.numStudenti();
+
+        corso1.ricercaNumMatricola(161002);
     }
+    
 }
